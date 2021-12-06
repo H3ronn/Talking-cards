cf1394386d5ab3e240ab19d28f82474d4152bd06
Bartosz Czaplicki <h3ronnn@gmail.com>
2021-12-07 00:21:56 +0100

    Add basic setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <ul>
+          <li>Card list</li>
+          <li>Create card</li >
+          <li>FAQ</li>
+          <li>Help</li>
+          <button>Login/Register</button>
+        </ul>
+      </nav>
+      <h1>Talking cards</h1>
     </div>
   );
-}
+};
 
 export default App;
