9151698f90cbe11f7690c9f962f4bd4cf0797007
Bartosz Czaplicki <h3ronnn@gmail.com>
2021-12-08 00:36:47 +0100

    Split styles from CreateCard view

--- src/views/CreateCard.js
+++ src/views/CreateCard.js
@@ -1,64 +1,9 @@
-import React, { useState, useRef, useEffect } from 'react';
-import styled from 'styled-components';
+import React, { useState, useRef } from 'react';
+import { Wrapper, CardWrapper, Caption, ImageWrapper, Image, StyledButton, ButtonsWrapper } from './CreateCard.styles';
 import InputField from 'components/molecules/InputField/InputField';
 import InputFile from 'components/atoms/InputFile/InputFile';
 import domtoimage from 'dom-to-image';
 import { saveAs } from 'file-saver';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-const CardWrapper = styled.div`
-  width: 450px;
-  height: 500px;
-  margin-top: 5px;
-  background-color: ${({ theme }) => theme.colors.blue};
-  display: grid;
-  grid-template-rows: 9fr 1fr;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Caption = styled.p`
-  text-align: center;
-`;
-
-const ImageWrapper = styled.div`
-  width: 400px;
-  height: 400px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const Image = styled.img`
-  max-width: 400px;
-`;
-
-const StyledButton = styled.button`
-  margin-left: 20px;
-  color: white;
-  background-color: ${({ theme }) => theme.colors.blue};
-  width: max-content;
-  padding: 8px 12px;
-  cursor: pointer;
-  border: none;
-  font-size: 20px;
-  transition: transform 0.2s ease-in-out;
-
-  &:hover,
-  &:focus {
-    transform: scale(1.05);
-  }
-`;
-
-const ButtonsWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 10px;
-`;
 
 const CreateCard = () => {
   const [caption, setCaption] = useState('Caption');
---
+++ src/views/CreateCard.styles.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export const CardWrapper = styled.div`
+  width: 450px;
+  height: 500px;
+  margin-top: 5px;
+  background-color: ${({ theme }) => theme.colors.blue};
+  display: grid;
+  grid-template-rows: 9fr 1fr;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Caption = styled.p`
+  text-align: center;
+`;
+
+export const ImageWrapper = styled.div`
+  width: 400px;
+  height: 400px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Image = styled.img`
+  max-width: 400px;
+`;
+
+export const StyledButton = styled.button`
+  margin-left: 20px;
+  color: white;
+  background-color: ${({ theme }) => theme.colors.blue};
+  width: max-content;
+  padding: 8px 12px;
+  cursor: pointer;
+  border: none;
+  font-size: 20px;
+  transition: transform 0.2s ease-in-out;
+
+  &:hover,
+  &:focus {
+    transform: scale(1.05);
+  }
+`;
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+`;
