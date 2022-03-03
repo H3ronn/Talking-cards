7691ab811898294acd1167efeb2ebbc541e29c68
Bartosz Czaplicki <h3ronnn@gmail.com>
2022-03-03 19:48:46 +0100

    Fix mobile style for Title component, Login and Register views

--- src/components/atoms/Title/Title.js
+++ src/components/atoms/Title/Title.js
@@ -4,6 +4,9 @@ const Title = styled.h1`
   margin: 15px;
   white-space: nowrap;
   color: ${({ theme }) => theme.colors.darkGrey};
+  @media (max-width: 400px) {
+    font-size: 1.5rem;
+  }
 `;
 
 export default Title;
