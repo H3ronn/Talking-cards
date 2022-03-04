2d9177782fe82ee95bbdd77700e19c0cf3253c46
Bartosz Czaplicki <h3ronnn@gmail.com>
2022-03-04 13:35:57 +0100

    Add useError hook and context

--- src/providers/AppProviders.js
+++ src/providers/AppProviders.js
@@ -4,16 +4,19 @@ import { ThemeProvider } from 'styled-components';
 import { theme } from 'assets/styles/theme';
 import { BrowserRouter } from 'react-router-dom';
 import CardProvider from 'providers/CardProvider';
+import { ErrorProvider } from 'hooks/useError';
 
 const AppProviders = ({ children }) => {
   return (
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CardProvider>
-          <GlobalStyles />
-          {children}
-        </CardProvider>
-      </ThemeProvider>
+      <ErrorProvider>
+        <ThemeProvider theme={theme}>
+          <CardProvider>
+            <GlobalStyles />
+            {children}
+          </CardProvider>
+        </ThemeProvider>
+      </ErrorProvider>
     </BrowserRouter>
   );
 };
