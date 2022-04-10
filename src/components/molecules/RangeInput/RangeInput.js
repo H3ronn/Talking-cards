be21ef91fd18e5eb67aa35a1ca25292d0b935f5f
Bartosz Czaplicki <h3ronnn@gmail.com>
2022-04-10 23:43:13 +0200

    Fix ControlButtons type

--- src/components/molecules/RangeInput/RangeInput.js
+++ src/components/molecules/RangeInput/RangeInput.js
@@ -8,10 +8,14 @@ const RangeInput = ({ value, label, id, name, unit = '', onChange, min = 0, max 
       <Label htmlFor={id}>{label}</Label>
       <Input type="range" id={id} name={name} min={min} max={max} value={value} step={step} onChange={onChange} />
       <span>
-        <ControlButton onClick={() => handleControls('DECREMENT', name)}>-</ControlButton>
+        <ControlButton type="button" onClick={() => handleControls('DECREMENT', name)}>
+          -
+        </ControlButton>
         {value}
         {unit}
-        <ControlButton onClick={() => handleControls('INCREMENT', name)}>+</ControlButton>
+        <ControlButton type="button" onClick={() => handleControls('INCREMENT', name)}>
+          +
+        </ControlButton>
       </span>
     </Wrapper>
   );
